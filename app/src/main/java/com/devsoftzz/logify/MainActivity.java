8703316ff695c9c6727ca0a09aa3e1b7dc1c6864
Deven <devsoft.deven@gmail.com>
2019-11-11 21:42:15 +0530

    UI Updated

--- app/src/main/java/com/devsoftzz/logify/MainActivity.java
+++ app/src/main/java/com/devsoftzz/logify/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.net.http.SslError;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.webkit.SslErrorHandler;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -21,6 +30,8 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,10 +40,18 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
+    private ImageView mRefresh;
     private SharedPreferences mStorage;
+    private LinearLayout mYES,mNO,mRotate;
     private String USERNAME,PASSWORD;
     private String URL = "https://10.100.56.55:8090/";
     private boolean second = false;
+    private Dialog dialog,dialogWIFI;
+    private TypeFaces typeFaces = new TypeFaces();
+    private char[][] values = typeFaces.getData();
+    private StringBuilder TempUSER = new StringBuilder();
+    private WifiManager wifi;
+    private Animation rotate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,21 +67,73 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+        mYES = findViewById(R.id.yes);
+        mNO = findViewById(R.id.no);
+        mNO.setVisibility(View.VISIBLE);
+        mYES.setVisibility(View.INVISIBLE);
+
         USERNAME = mStorage.getString("username","");
         PASSWORD = mStorage.getString("password","");
 
+
+        mRotate = findViewById(R.id.rotation);
+
         loadSite();
+
+        mRefresh = findViewById(R.id.refresh);
+        mRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshView();
+            }
+        });
+
+        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if(wifi.getWifiState()==WifiManager.WIFI_STATE_DISABLED){
+            wifi.setWifiEnabled(true);
+            showWIFIDialog();
+        }
 
     }
 
-    private void loadSite() {
+    private void showWIFIDialog() {
 
+        dialogWIFI = new Dialog(this);
+        dialogWIFI.setContentView(R.layout.wifi_pop);
+        dialogWIFI.setCanceledOnTouchOutside(true);
+        TextView done = dialogWIFI.findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadSite();
+                dialogWIFI.dismiss();}});
+        dialogWIFI.show();
+    }
+
+    private void refreshView() {
+        rotateButton();
+        second=false;
         generateUSER();
+        mWebView.loadUrl(URL);
+    }
+
+    private void loadSite() {
+        rotateButton();
+        generateUSER();
+        configureWebView();
+        mWebView.loadUrl(URL);
+    }
+
+    private void rotateButton() {
+        rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.round);
+        mRotate.startAnimation(rotate);
+    }
+
+    private void configureWebView() {
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-        mWebView.loadUrl(URL);
         mWebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
@@ -70,27 +141,35 @@ public class MainActivity extends AppCompatActivity {
             }
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                String js = "javascript:" +
+                        "document.getElementById('password').value = '" + PASSWORD + "';"  +
+                        "document.getElementById('username').value = '" + TempUSER.toString() + "';";
 
-                    String js="";
+                if(!second){
+                    js += "document.getElementById('loginbutton').click()";
+                    second=true;
+                }
 
-                    if(!second){
-                         js += "javascript:" +
-                                "document.getElementById('password').value = '" + PASSWORD + "';"  +
-                                "document.getElementById('username').value = '" + USERNAME + "';"  +
-                                "document.getElementById('loginbutton').click()";
-                        second=true;
-                    }else {
-                        js += "javascript:" +
-                                "document.getElementById('password').value = '" + PASSWORD + "';"  +
-                                "document.getElementById('username').value = '" + USERNAME + "';";
-                    }
-                    view.evaluateJavascript(js, new ValueCallback<String>() {
-                        @Override
-                        public void onReceiveValue(String s) {
+                view.evaluateJavascript(js, new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String s) {
+                        if(!s.equals("null")){
+                            mYES.setVisibility(View.VISIBLE);
+                            mNO.setVisibility(View.INVISIBLE);
+                        }else {
+                            mNO.setVisibility(View.VISIBLE);
+                            mYES.setVisibility(View.INVISIBLE);
                         }
-                    });
-            }
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRotate.clearAnimation();
+                            }
+                        },1500);
 
+                    }
+                });
+            }
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
                 handler.proceed();
@@ -98,60 +177,65 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){
                 Toast.makeText(getApplicationContext(), "Something Want Wrong", Toast.LENGTH_LONG).show();
+                mNO.setVisibility(View.VISIBLE);
+                mYES.setVisibility(View.INVISIBLE);
             }
         });
     }
+
     private void generateUSER() {
 
-        TypeFaces typeFaces = new TypeFaces();
-        char[][] values = typeFaces.getData();
-        int token;
-        StringBuilder TempUSER = new StringBuilder();
+        TempUSER.delete(0,TempUSER.length());
+
         String RANDOM = String.valueOf(new Random().nextInt(900000000) + 100000000);
-
-        //Toast.makeText(getApplicationContext(),String.valueOf(values.length),Toast.LENGTH_LONG).show();
-
         for (int i=0;i<USERNAME.length();i++){
-            token = (int) RANDOM.charAt(i)%values.length;
-            switch (USERNAME.charAt(i)){
-                case '0':
-                    TempUSER.append(values[token][0]);
-                    break;
-                case '1':
-                    TempUSER.append(values[token][1]);
-                    break;
-                case '2':
-                    TempUSER.append(values[token][2]);
-                    break;
-                case '3':
-                    TempUSER.append(values[token][3]);
-                    break;
-                case '4':
-                    TempUSER.append(values[token][4]);
-                    break;
-                case '5':
-                    TempUSER.append(values[token][5]);
-                    break;
-                case '6':
-                    TempUSER.append(values[token][6]);
-                    break;
-                case '7':
-                    TempUSER.append(values[token][7]);
-                    break;
-                case '8':
-                    TempUSER.append(values[token][8]);
-                    break;
-                case '9':
-                    TempUSER.append(values[token][9]);
-                    break;
-
-                    default:
-                        TempUSER.append(USERNAME.charAt(i));
-
-            }
+            randomChar(i,RANDOM.charAt(i));
         }
-        USERNAME = TempUSER.toString();
     }
+
+    private void randomChar(int i,char rand) {
+
+        int token;
+        token = (int) rand%values.length;
+        switch (USERNAME.charAt(i)){
+            case '0':
+                TempUSER.append(values[token][0]);
+                break;
+            case '1':
+                TempUSER.append(values[token][1]);
+                break;
+            case '2':
+                TempUSER.append(values[token][2]);
+                break;
+            case '3':
+                TempUSER.append(values[token][3]);
+                break;
+            case '4':
+                TempUSER.append(values[token][4]);
+                break;
+            case '5':
+                TempUSER.append(values[token][5]);
+                break;
+            case '6':
+                TempUSER.append(values[token][6]);
+                break;
+            case '7':
+                TempUSER.append(values[token][7]);
+                break;
+            case '8':
+                TempUSER.append(values[token][8]);
+                break;
+            case '9':
+                TempUSER.append(values[token][9]);
+                break;
+
+            default:
+                TempUSER.append(USERNAME.charAt(i));
+
+        }
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.side_menu, menu);
@@ -159,53 +243,74 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
+
             case R.id.credentials:
                 //Toast.makeText(getApplicationContext(),"Credentials",Toast.LENGTH_LONG).show();
                 startActivity(new Intent(MainActivity.this,Credentials.class));
                 return true;
+
             case R.id.info:
                 //Toast.makeText(getApplicationContext(),"Info",Toast.LENGTH_LONG).show();
-                final Dialog dialog = new Dialog(this);
-                dialog.setContentView(R.layout.activity_info);
-                dialog.setCanceledOnTouchOutside(true);
-                TextView wp = dialog.findViewById(R.id.wp);
-                TextView github = dialog.findViewById(R.id.code);
-
-                wp.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if(isWPInstalled()){
-                            Uri mUri = Uri.parse("smsto:+919824978996");
-                            Intent mIntent = new Intent(Intent.ACTION_SENDTO, mUri);
-                            mIntent.setPackage("com.whatsapp");
-                            mIntent.putExtra("chat",true);
-                            startActivity(mIntent);
-                            dialog.dismiss();
-                        }else {
-                            Toast.makeText(getApplicationContext(),"WhatsApp isn't Installed.",Toast.LENGTH_LONG).show();
-                            dialog.dismiss();
-                        }
-                    }
-                });
-                github.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String url = "https://github.com/devsoftzz/Logify";
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        intent.setData(Uri.parse(url));
-                        startActivity(intent);
-                        dialog.dismiss();
-                    }
-                });
-
-                dialog.show();
+                showInfo();
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
+
         }
+
     }
+
+    private void showInfo() {
+
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.activity_info);
+        dialog.setCanceledOnTouchOutside(true);
+        TextView wp = dialog.findViewById(R.id.wp);
+        TextView github = dialog.findViewById(R.id.code);
+        wp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectWhatsApp();}});
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webURL(); }});
+        dialog.show();
+
+    }
+
+    private void webURL() {
+
+        String url = "https://github.com/devsoftzz/Logify";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+        dialog.dismiss();
+
+    }
+
+    private void redirectWhatsApp() {
+        if(isWPInstalled()){
+
+            Uri mUri = Uri.parse("smsto:+919824978996");
+            Intent mIntent = new Intent(Intent.ACTION_SENDTO, mUri);
+            mIntent.setPackage("com.whatsapp");
+            mIntent.putExtra("chat",true);
+            startActivity(mIntent);
+
+        }else {
+
+            Toast.makeText(getApplicationContext(),"WhatsApp isn't Installed.",Toast.LENGTH_LONG).show();
+
+        }
+        dialog.dismiss();
+    }
+
     private boolean isWPInstalled() {
+
         PackageManager pm = getPackageManager();
         boolean app_installed;
         try {
@@ -216,5 +321,6 @@ public class MainActivity extends AppCompatActivity {
             app_installed = false;
         }
         return app_installed;
+
     }
 }
